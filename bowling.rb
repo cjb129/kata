74ba4e5dd20cc0dc00bda1a1f2c84599f7bcb452
Cory <cjb129@zips.uakron.edu>
2016-12-06 16:47:13 -0500

    Second test

--- bowling.rb
+++ bowling.rb
@@ -3,11 +3,21 @@ gem 'test-unit'
 
 class Game
   def initialize
-    @arr = Array.new()
+    @score = Array.new
   end
+
   def Score()
-    return 0
+    totalScore = 0
+    for pins in @score
+      totalScore+=pins
+    end
+    return totalScore
   end
+
+  def Roll(pins)
+    @score.push(pins)
+  end
+
 end
 
 require 'test/unit'
@@ -16,5 +26,11 @@ class GameTest < Test::Unit::TestCase
   def testNoRollsReturnsZero()
     g = Game.new
     assert_equal 0, g.Score
+  end
+
+  def testRollOneReturnsOne
+    g = Game.new
+    g.Roll(1)
+    assert_equal 1, g.Score
   end
 end
