d1244f8ea0d96d44b90bce064d86e6f4642cbde7
Cory <cjb129@zips.uakron.edu>
2016-12-07 10:10:12 -0500

    assertions would be nice

--- binarySearch.rb
+++ binarySearch.rb
@@ -1,14 +1,22 @@
 require 'rubygems'
 require 'set'
 def search(numberToFind, set)
+  for number in set
+    if number == numberToFind
+      return true
+    end
+  end
   return -1
 end
 
 require 'test/unit'
 class SearchTest < Test::Unit::TestCase
-  def test_empty_array_returns_nothing
-    search(nil, SortedSet.new)
+  def test_empty_set_returns_nothing
+    assert_equal -1, search(nil, SortedSet.new)
   end
 
+  def test_search_single_element_that_exists
+    assert_equal true, search(1, SortedSet.new([1]))
+  end
 
 end
