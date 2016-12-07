c7984fb2c061a6d4e6b5485dfe4674e54af1de4e
Cory <cjb129@zips.uakron.edu>
2016-12-07 10:44:32 -0500

    change back to array

--- binarySearch.rb
+++ binarySearch.rb
@@ -1,22 +1,27 @@
 require 'rubygems'
 require 'set'
-def search(numberToFind, set)
-  for number in set
-    if number == numberToFind
+def search(numberToFind, array)
+  index = array.length / 2
+  for number in array
+    if array[index] == numberToFind
       return true
     end
   end
-  return -1
+  return false
 end
 
 require 'test/unit'
 class SearchTest < Test::Unit::TestCase
   def test_empty_set_returns_nothing
-    assert_equal -1, search(nil, SortedSet.new)
+    assert_equal false, search(nil, Array.new)
   end
 
-  def test_search_single_element_that_exists
-    assert_equal true, search(1, SortedSet.new([1]))
+  def test_single_element_set_that_that_includes_param
+    assert_equal true, search(1, Array.new([1]))
+  end
+
+  def test_single_element_set_that_does_not_include_param
+    assert_equal false, search(1, Array.new([2]))
   end
 
 end
