78c50e7d1b1bbea820017f5a469d09c45577a392
Cory <cjb129@zips.uakron.edu>
2016-12-07 11:12:23 -0500

    first red test

--- binarySearch.rb
+++ binarySearch.rb
@@ -2,26 +2,30 @@ require 'rubygems'
 require 'set'
 def search(numberToFind, array)
   index = array.length / 2
-  for number in array
-    if array[index] == numberToFind
-      return true
-    end
+
+  if array[index] == numberToFind
+    return index
   end
-  return false
+
+  return -1
 end
 
 require 'test/unit'
 class SearchTest < Test::Unit::TestCase
-  def test_empty_set_returns_nothing
-    assert_equal false, search(nil, Array.new)
+  def test_empty_array_returns_nothing
+    assert_equal -1, search(1, Array.new)
   end
 
-  def test_single_element_set_that_that_includes_param
-    assert_equal true, search(1, Array.new([1]))
+  def test_single_element_array_that_that_includes_param
+    assert_equal 0, search(1, Array.new([1]))
   end
 
-  def test_single_element_set_that_does_not_include_param
-    assert_equal false, search(1, Array.new([2]))
+  def test_single_element_array_that_does_not_include_param
+    assert_equal -1, search(1, Array.new([2]))
+  end
+
+  def test_two_element_array_that_includes_param_in_first_half
+    assert_equal 0, search(1, Array.new([1,2]))
   end
 
 end
