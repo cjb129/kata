e6541b7ccbecbe0c8a9313143a122accb2ff019e
Cory <cjb129@zips.uakron.edu>
2016-12-07 13:11:01 -0500

    true false works for up to 4 elements

--- binarySearch.rb
+++ binarySearch.rb
@@ -1,8 +1,9 @@
 require 'rubygems'
 require 'set'
+
 def search(numberToFind, array)
   index = (array.length - 1) / 2
-  puts index
+
   if array[index] == numberToFind
     return true
   end
@@ -10,7 +11,11 @@ def search(numberToFind, array)
     return false
   end
 
-  return search(numberToFind, array.drop(index+1))
+  if array[index] < numberToFind
+    search(numberToFind, array.drop(index+1))
+  else
+    search(numberToFind, array.take(index))
+  end
 end
 
 require 'test/unit'
@@ -33,5 +38,45 @@ class SearchTest < Test::Unit::TestCase
 
   def test_two_element_array_that_includes_param_in_second_half
     assert_equal true, search(2, Array.new([1,2]))
+  end
+
+  def test_two_element_array_where_param_does_not_exist
+    assert_equal false, search(0, Array.new([1,2]))
+  end
+
+  def test_three_element_array_where_param_is_middle_element
+    assert_equal true, search(1, Array.new([0,1,2]))
+  end
+
+  def test_three_element_array_with_param_in_first_half
+    assert_equal true, search(0, Array.new([0,1,2]))
+  end
+
+  def test_three_element_array_with_param_in_second_half
+    assert_equal true, search(2, Array.new([0,1,2]))
+  end
+
+  def test_three_element_array_where_param_does_not_exist
+    assert_equal false, search(5, Array.new([0,1,2]))
+  end
+
+  def test_four_element_array_v1
+    assert_equal true, search(0, Array.new([0,1,2,3]))
+  end
+
+  def test_four_element_array_v2
+    assert_equal true, search(1, Array.new([0,1,2,3]))
+  end
+
+  def test_four_element_array_v3
+    assert_equal true, search(2, Array.new([0,1,2,3]))
+  end
+
+  def test_four_element_array_v4
+    assert_equal true, search(3, Array.new([0,1,2,3]))
+  end
+
+  def test_four_element_array_where_param_does_not_exist
+    assert_equal false, search(4, Array.new([0,1,2,3]))
   end
 end
