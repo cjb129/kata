bb2d40eb71d004bf791c511585b3ea64c55a6c80
Cory <cjb129@zips.uakron.edu>
2016-12-07 10:02:54 -0500

    change to set, figure out syntax

--- binarySearch.rb
+++ binarySearch.rb
@@ -1,11 +1,14 @@
 require 'rubygems'
-def search(numberToFind, sortedArray)
+require 'set'
+def search(numberToFind, set)
   return -1
 end
 
 require 'test/unit'
 class SearchTest < Test::Unit::TestCase
   def test_empty_array_returns_nothing
-    search(nil, Array.new)
+    search(nil, SortedSet.new)
   end
+
+
 end
